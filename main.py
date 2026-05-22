daeba7dddd89cc67ad8cd70773120d4835c7b97c
anonymous <anonymous@email.com>
2026-05-22 13:10:11 +0200

    Wolken funktionieren, ordner mit Monster Bildern hinzugefügt

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 import pgzrun
+import random
 
 WIDTH = 1280
 HEIGHT = 720
 
-cloud = Actor("cloud1")
-cloud.vx = 1
-cloud.pos = (0,200)
+#cloud = Actor("cloud1")
+#cloud.vx = 1
+#cloud.pos = (0,200)
+
+clouds = [
+    Actor('cloud1', topleft=(0,0)),
+    Actor('cloud2', topleft=(200,50)),
+    Actor('cloud3', topleft=(500,25))
+]
+
+for i in range(3):
+    clouds[i].vx = i+1
 
 #Update
 def update():
-    if cloud.left <= WIDTH:
-        cloud.left += cloud.vx
-    else:
-        cloud.left = -300
-        
+    for cloud in clouds:
+        if cloud.left <= WIDTH:
+            cloud.left += cloud.vx
+        else:
+            cloud.left = -300
+            cloud.vx = random.randint(1,3)
+
 
    
     
@@ -21,5 +33,6 @@ def update():
 
 def draw():
     screen.blit("backgroundcolorforest",(0,0))
-    cloud.draw()
+    for cloud in clouds:
+        cloud.draw()
 pgzrun.go()
